81310bbdec4802ed53673cc6774e9b7b7abf9e1f
chrishendra93 <chrishendra93@gmail.com>
2017-12-19 20:22:21 +0800

    standardise fit method

--- classifiers/bayesian_network/bayesian_network.py
+++ classifiers/bayesian_network/bayesian_network.py
@@ -5,20 +5,31 @@ from classifiers.bayesian_network.intermediate_results import IntermediateResult
 
 class BayesianNetwork(object):
 
-    def __init__(self, X, features, graph={}, features_type={}):
+    def __init__(self):
+        self.training_df = []
+        self.features = []
+        self.graph = []
+        self.features_type = []
+        self.intermediate_results = []
+        self.nodes = []
+        self.class_label = "class_label"
+
+    def fit(self, X, y, graph={}, features_type={}):
         self.training_df = X
-        self.features = features
+        self.features = X.columns + self.class_label
         self.graph = graph
+
         self.features_type = features_type
+        self.features_type.update({self.class_label: 'd'})
+
         self.intermediate_results = IntermediateResults(X)
         self.nodes = {}
 
-    def fit(self):
-        if len(self.graph) == 0:
-            raise ValueError("no graph structure deteceted")
+        if len(self.graph) != 0:
 
-        for var, par in self.graph.iteritems():
-            self.nodes[var] = Node(self.training_df, var, par, self.intermediate_results, self.features_type)
+            for var, par in self.graph.iteritems():
+                self.nodes[var] = Node(self.training_df, var, par, self.intermediate_results,
+                                       self.features_type)
 
     def compute_ll(self, X):
         ll = np.zeros(len(X))
--- classifiers/bayesian_network/chow_liu_tree.py
+++ classifiers/bayesian_network/chow_liu_tree.py
@@ -8,16 +8,17 @@ from itertools import combinations
 
 class ChowLiuTree(BayesianNetwork):
 
-    def __init__(self, X, features, graph={}, features_type={}):
-        super(ChowLiuTree, self).__init__(X, features, graph, features_type)
+    def __init__(self):
+        super(ChowLiuTree, self).__init__()
 
-    def fit(self):
-        vertices, edges = self.construct_mi_graphs()
-        mst = Kruskal({"vertices": vertices, "edges": edges}, mode="max").get_mst()
-        edge_list = convert_mst_to_edge_list(mst)
-        graph_dict = convert_edge_list_to_dict(edge_list, self.features)
-        self.graph = create_dependencies_dict(graph_dict, self.features, self.class_label)
-        super(ChowLiuTree, self).fit()
+    def fit(self, X, y, graph={}, features_type={}):
+        super(ChowLiuTree, self).fit(X, y, graph, features_type)
+        if len(graph) == 0:
+            vertices, edges = self.construct_mi_graphs()
+            mst = Kruskal({"vertices": vertices, "edges": edges}, mode="max").get_mst()
+            edge_list = convert_mst_to_edge_list(mst)
+            graph_dict = convert_edge_list_to_dict(edge_list, self.features)
+            self.graph = create_dependencies_dict(graph_dict, self.features, self.class_label)
 
     def construct_mi_graphs(self):
         vertices = self.features
